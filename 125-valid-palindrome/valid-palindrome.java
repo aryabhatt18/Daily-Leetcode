81c07a548a09ed5e803fc952c7e7887aaf03b2c0
Abhishek Bhattacharya <93682713+aryabhatt18@users.noreply.github.com>
2026-08-09 01:50:41 +0530

    Time: 137 ms (5.16%) | Memory: 47.5 MB (11.17%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+         String str = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (Character.isLetterOrDigit(ch)) {
+                str += Character.toLowerCase(ch);
+            }
+        }
+
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while(i < j)
+        {
+            if(str.charAt(i) == str.charAt(j))
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
